5579da852c889dcfa0c2f4c6df34c3409824a1eb
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-03-23 01:25:25 -0400

    Fixed locales

---
+++ locales/en.lua
@@ -0,0 +1,20 @@
+Locales['en'] = {
+    ["open_tow_menu"]             = "Press ~INPUT_LOOK_BEHIND~ to open the towing menu",
+    ["attach_vehicle"]            = "Press ~INPUT_LOOK_BEHIND~ to attach the vehicle",
+    ["detach_vehicle"]            = "Press ~INPUT_LOOK_BEHIND~ to detach the vehicle",
+	["front_car_pjtrailer"]       = "Tow/Untow vehicle front",
+	["middle_car_pjtrailer"]      = "Tow/Untow vehicle middle",
+	["back_car_pjtrailer"]        = "Tow/Untow vehicle back",
+	["open_rampe_pjtrailer"]      = "Open the ramp",
+	["close_rampe_pjtrailer"]     = "Close the ramp",
+	["open_rampe"]                = "Open the lower ramp",
+	["close_rampe"]               = "Close the lower ramp",
+	["close_plateau"]             = 'Close the upper ramp',
+    ["open_plateau"]              = 'Open the upper ramp',
+    ["up_front_car"]              = 'Tow/Untow vehicle lower front',
+    ["up_middle_car"]             = 'Tow/Untow vehicle lower middle',
+    ["up_back_car"]               = 'Tow/Untow vehicle lower back',
+    ["down_front_car"]            = 'Tow/Untow vehicle upper front',
+    ["down_middle_car"]           = 'Tow/Untow vehicle upper middle',
+    ["down_back_car"]             = 'Tow/Untow vehicle upper back'
+}
