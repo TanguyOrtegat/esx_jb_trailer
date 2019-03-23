29fffef5657842f647a267ae69881dbb4993cfaf
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-03-23 01:24:16 -0400

    Locales Fixed

--- __resource.lua
+++ __resource.lua
@@ -1,10 +1,13 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'trailer script by Jager Bom'
+description 'trailer script by Jager Bom & Edited by XxFri3ndlyxX'
 
-version '1.0.0'
+version '1.1.0'
 
 client_scripts {
+  '@es_extended/locale.lua',
+  'locales/en.lua',
+  'locales/fr.lua',
   'config.lua',
   'trailerclientmain.lua'
 }
